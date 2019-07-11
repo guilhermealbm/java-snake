3335bde3bd70648e977b832c5565866976639259
guilhermealbm <gui.miranda.13@gmail.com>
2019-07-11 00:05:55 -0300

    adding arrows movement

--- snake/src/snake/Gamescreen.java
+++ snake/src/snake/Gamescreen.java
@@ -8,6 +8,8 @@ package snake;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,7 +19,7 @@ import javax.swing.JPanel;
  *
  * @author guilherme
  */ 
-public class Gamescreen extends JPanel implements Runnable{
+public class Gamescreen extends JPanel implements Runnable, KeyListener{
     
      
     public static final int WIDTH = 400, HEIGHT = 400;
@@ -30,12 +32,14 @@ public class Gamescreen extends JPanel implements Runnable{
     private int x = 10, y = 10, size = 5;
     
     private boolean right = true, left = false, up = false, down = false;
-    
     private int ticks = 0;
     
-    public  Gamescreen(){
+    public Gamescreen(){
+        
+        setFocusable(true);
         
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        addKeyListener(this);
         snake = new Vector<Body>();
         start();
         
@@ -59,7 +63,7 @@ public class Gamescreen extends JPanel implements Runnable{
     
     public void tick(){
         
-        if(snake.size() == 0){ //Create new snake
+        if(snake.isEmpty()){ //Create new snake
             b = new Body(x, y, 10);
             snake.add(b);
         }
@@ -71,18 +75,15 @@ public class Gamescreen extends JPanel implements Runnable{
         if(left) x--;
         if(up) y--;
         if(down) y++;
-
-        //ticks = 0;
-
+        
         b = new Body(x, y, 10);
         snake.add(b);
 
         
         try {
-            Thread.sleep(100);
+            Thread.sleep(100); //Change for difficulty
             if(snake.size() > size){
-                System.out.println(ticks);
-                snake.remove(0);
+                snake.remove(0);//Movement
             }
         } catch (InterruptedException ex) {
                 Logger.getLogger(Gamescreen.class.getName()).log(Level.SEVERE, null, ex);
@@ -96,6 +97,7 @@ public class Gamescreen extends JPanel implements Runnable{
         g.setColor(Color.gray);
         g.fillRect(0, 0, WIDTH, HEIGHT);
         
+        //each cell = 10px x 10px
         for (int i = 0; i < WIDTH/10; i++) {
             g.drawLine(i*10, 0, i*10, HEIGHT);
         }
@@ -117,6 +119,39 @@ public class Gamescreen extends JPanel implements Runnable{
             repaint();
         }
         
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        
+        if((key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) && !left){
+            right = true;
+            up = false;
+            down = false;
+        }else if((key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) && !right){
+            left = true;
+            up = false;
+            down = false;
+        }else if((key == KeyEvent.VK_UP || key == KeyEvent.VK_W) && !down){
+            up = true;
+            left = false;
+            right = false;
+        }else if((key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S) && !up){
+            down = true;
+            left = false;
+            right = false;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
     }
     
 }
